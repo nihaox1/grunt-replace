d15e61bb188cf29619159a726f43c62790b8cd0d
bo01.cheng@vipshop.com <bo01.cheng@vipshop.com>
2014-06-18 17:37:21 +0800

    update

--- dev/core/FQAStr.js
+++
@@ -1,9 +0,0 @@
-$c.define( {
-	page 		: $g.config.FQA,
-	constructor : function() {
-		var str = {"error":0,"data":[{"id":23,"type":"FQA","title":"\u4e3a\u4ec0\u4e48\u6211\u4e0b\u8f7d\u7684\u4ee3\u7801 index.html \u6253\u5f00 \u6ca1\u6709\u5c55\u73b0\u5185\u5bb9","des":"\u4e3a\u4ec0\u4e48\u6211\u4e0b\u8f7d\u7684\u4ee3\u7801 index.html \u6253\u5f00 \u6ca1\u6709\u5c55\u73b0\u5185\u5bb9","content":"<p>casync.js\u4f7f\u7528\u7684\u573a\u666f\u8981\u6c42\u4e00\u4e2a\u7f51\u7edc\u7684\u5f00\u53d1\u73af\u5883 apache\u3001nginx\u3001nodejs\u7b49\u7b49<\/p>\n<p>\u5426\u5219 \u65e0\u6cd5\u5bf9html\u6587\u4ef6\u63d0\u4f9b\u652f\u6301<\/p>","create_time":1401171973,"last_modify_time":1401171973}]};		
-		return {
-			FQA 	: str.data
-		};
-	}
-} );
